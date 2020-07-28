e2ff7bcfdc6c7a46e419c3937054991b3c33daef
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-28 18:05:08 -0400

    commented out the logger

--- server.js
+++ server.js
@@ -7,7 +7,7 @@ const PORT = process.env.PORT || 3000;
 
 const app = express();
 
-app.use(logger("dev"));
+// app.use(logger("dev"));
 
 app.use(compression());
 app.use(express.urlencoded({ extended: true }));
