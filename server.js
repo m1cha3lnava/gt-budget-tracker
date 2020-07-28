bde9932ab8c120a6c9e7a4562792f8b085accb96
Michael Nava <M1cha3l.nava@gmail.com>
2020-07-28 17:54:31 -0400

    added the unified topology

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const logger = require("morgan");
+// const logger = require("morgan");
 const mongoose = require("mongoose");
 const compression = require("compression");
 
@@ -17,7 +17,8 @@ app.use(express.static("public"));
 
 mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
@@ -25,4 +26,5 @@ app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
   console.log(`App running on port ${PORT}!`);
+  console.log("http://localhost:3000/")
 });
